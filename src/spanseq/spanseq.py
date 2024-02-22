3e06a885586b4bc3d1e874e32f67f96ba87c4755
Alfred Ferrer Florensa <30677070+ffalfred@users.noreply.github.com>
2024-02-22 14:44:35 +0100

    Update spanseq with copyright

--- src/spanseq/spanseq.py
+++ src/spanseq/spanseq.py
@@ -1,3 +1,11 @@
+#    Copyright (c) 2023 Technical University of Denmark
+#    All rights reserved.
+
+ #   This source code is licensed under the BSD-style license found in the
+ #   LICENSE file in the root directory of this source tree. 
+
+
+
 import argparse
 import subprocess
 import os
